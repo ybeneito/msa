fba0020da31bc993d510e847e00487bbed1922a0
Dorian AMCHi <d.amchi@majelis.net>
2021-10-14 16:16:25 +0200

    debut du cookie

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { BaseKonnector, log, requestFactory, scrape } = require('cozy-konnector-libs')
 
 const CozyBrowser = require('cozy-konnector-libs/dist/libs/CozyBrowser')
+const signin = require('cozy-konnector-libs/dist/libs/signin')
 const browser = new CozyBrowser({
   waitDuration: '5s'
 })
@@ -9,14 +10,17 @@ const browser = new CozyBrowser({
 const request = requestFactory({
   cheerio: true,
   json: false,
-  jar: true
+  jar: true,
+  debug: true
 })
 
 
 
 const baseUrl = "https://monespaceprive.msa.fr/lfy/web/msa"
 
-const coUrl = baseUrl + "/accueil?modalId=2"
+const testUrl = "https://monespaceprive.msa.fr/z84authmsa/wsrest/auth"
+
+const coUrl = "https://monespaceprive.msa.fr/lfy/web/msa/accueil?modalId=2"
 
 const roleUrl = "https://monespaceprive.msa.fr/lfy/web/msa-ain-rhone/accueil?modalId=5"
 
@@ -29,9 +33,10 @@ const particulier = "https://monespaceprive.msa.fr/lfy/group/espace-particuliers
 module.exports = new BaseKonnector(start)
   log('debug', 'hey')
   async function start(fields) {
-    await authenticate(fields.login, fields.password)
+    await this.deactivateAutoSuccessfulLogin()
+    await authenticate.bind(this)(fields.login, fields.password)
     log('info', 'set role')
-    await setRole(fields.role)
+  //  await setRole(fields.role)
 
 }
 
@@ -39,18 +44,23 @@ async function authenticate(username, password) {
   log('debug', 'auth')
   const $ = await request({
     method: 'POST',
-    uri: coUrl,
+    url: testUrl,
+    formSelector: "form",
     form: {
-      _connexionauthent_WAR_z80techrwdportlet__58_login: username,
-      _connexionauthent_WAR_z80techrwdportlet__58_password: password
+      "login": username,
+      "password": password
     },
+    //followRedirects: false,
+    followAllRedirects: true,
     resolveWithFullResponse: true
   })
     .catch(err => {
       log('err', err)
     })
     .then(resp => {
-     // log('info', resp)
+      log('debug',"response:   " + resp.request.uri)
+      const cookies = browser.cookies.get("Z84AUTHROUTEID")
+      log('debug', cookies)
       return resp
     })
 }
@@ -64,7 +74,4 @@ async function setRole(role){
 
 
 }
-
-
-
 
---
+++ src/old.js
@@ -0,0 +1,79 @@
+const {
+  BaseKonnector,
+  log,
+  requestFactory,
+  scrape,
+  signin
+ } = require('cozy-konnector-libs')
+
+
+const request = requestFactory({
+  debug: true,
+  cheerio: true,
+  json: false,
+  jar: true,
+  followAllRedirects: true
+})
+
+
+const coUrl = "https://monespaceprive.msa.fr/z84authmsa/wsrest/auth"
+
+const roleUrl = "https://monespaceprive.msa.fr/lfy/web/msa/accueil?p_p_id=choisirespace_WAR_z80techrwdportlet&p_p_lifecycle=0&p_p_state=exclusive&p_p_mode=view"
+
+
+
+module.exports = new BaseKonnector(start)
+
+
+  async function start(fields, cozyParameters) {
+   log('info', 'Authentification ... ')
+   if(cozyParameters) log('debug', 'Paramètres trouvés')
+   await authenticate.bind(this)(fields.login, fields.password)
+   log('info', 'Connexion ok')
+   log('info', 'set role')
+  //await setRole(fields.role)
+
+}
+
+
+
+async function authenticate(username, password) {
+  log('debug', 'auth')
+
+  const options = {
+    method: "POST",
+    url: coUrl,
+    formData: {
+      "_connexionauthent_WAR_z80techrwdportlet__58_login": username,
+      "_connexionauthent_WAR_z80techrwdportlet__58_password": password
+    },
+   // 'X-Requested-With': 'XMLHttpRequest'
+  };
+
+  try {
+    const response = await request(options)
+    log('info', "response" + response)
+    const $ = cheerio.load(response)
+    const msg = $('.error').text()
+    log('debug', "msg:     " + msg)
+  }
+  catch (err) {
+    log('debug', "err" + err)
+  }
+}
+
+
+async function setRole(role){
+  log('debug', role)
+
+  const $ = await request({
+    method: 'GET',
+    uri: roleUrl
+  })
+  log('info',$)
+
+}
+
+
+
+
