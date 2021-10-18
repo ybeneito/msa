9303d7a9db5b7df68a86a1a0352a3efe644d53f0
Dorian AMCHi <d.amchi@majelis.net>
2021-10-18 16:50:49 +0200

    lundi soir

--- src/old.js
+++ src/old.js
@@ -4,8 +4,7 @@ const {
   requestFactory,
   scrape,
   signin
- } = require('cozy-konnector-libs')
-
+} = require('cozy-konnector-libs')
 
 const request = requestFactory({
   debug: true,
@@ -15,65 +14,52 @@ const request = requestFactory({
   followAllRedirects: true
 })
 
+const coUrl = 'https://monespaceprive.msa.fr/z84authmsa/wsrest/auth'
 
-const coUrl = "https://monespaceprive.msa.fr/z84authmsa/wsrest/auth"
-
-const roleUrl = "https://monespaceprive.msa.fr/lfy/web/msa/accueil?p_p_id=choisirespace_WAR_z80techrwdportlet&p_p_lifecycle=0&p_p_state=exclusive&p_p_mode=view"
-
-
+const roleUrl =
+  'https://monespaceprive.msa.fr/lfy/web/msa/accueil?p_p_id=choisirespace_WAR_z80techrwdportlet&p_p_lifecycle=0&p_p_state=exclusive&p_p_mode=view'
 
 module.exports = new BaseKonnector(start)
 
-
-  async function start(fields, cozyParameters) {
-   log('info', 'Authentification ... ')
-   if(cozyParameters) log('debug', 'Paramètres trouvés')
-   await authenticate.bind(this)(fields.login, fields.password)
-   log('info', 'Connexion ok')
-   log('info', 'set role')
-  //await setRole(fields.role)
-
+async function start(fields, cozyParameters) {
+  log('info', 'Authentification ... ')
+  if (cozyParameters) log('debug', 'Paramètres trouvés')
+  await authenticate.bind(this)(fields.login, fields.password)
+  log('info', 'Connexion ok')
+  log('info', 'set role')
+  // await setRole(fields.role)
 }
-
-
 
 async function authenticate(username, password) {
   log('debug', 'auth')
 
   const options = {
-    method: "POST",
+    method: 'POST',
     url: coUrl,
     formData: {
-      "_connexionauthent_WAR_z80techrwdportlet__58_login": username,
-      "_connexionauthent_WAR_z80techrwdportlet__58_password": password
-    },
-   // 'X-Requested-With': 'XMLHttpRequest'
-  };
+      _connexionauthent_WAR_z80techrwdportlet__58_login: username,
+      _connexionauthent_WAR_z80techrwdportlet__58_password: password
+    }
+    // 'X-Requested-With': 'XMLHttpRequest'
+  }
 
   try {
     const response = await request(options)
-    log('info', "response" + response)
+    log('info', 'response' + response)
     const $ = cheerio.load(response)
     const msg = $('.error').text()
-    log('debug', "msg:     " + msg)
-  }
-  catch (err) {
-    log('debug', "err" + err)
+    log('debug', 'msg:     ' + msg)
+  } catch (err) {
+    log('debug', 'err' + err)
   }
 }
 
-
-async function setRole(role){
+async function setRole(role) {
   log('debug', role)
 
   const $ = await request({
     method: 'GET',
     uri: roleUrl
   })
-  log('info',$)
-
+  log('info', $)
 }
-
-
-
-
